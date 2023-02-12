820fc70b77b99452434d11582e6f395028ee1a22
zhuanghj <zhuanghj@landray.com.cn>
2023-02-12 13:49:20 +0800

    feat: deploy webhook

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='App'>
-      dddddddd test dev
+        Hello Deploy
       <button type='primary' onClick={() => alert('button click')}>
         Button
       </button>
