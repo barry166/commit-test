8d1b21109feaa7aa4d781cc13fdbece229a2fdf6
zhuanghj <zhuanghj@landray.com.cn>
2023-02-12 16:30:58 +0800

    feat: deploy webhook test2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div className='App'>
         Hello Deploy...
+        this is test...
       <button type='primary' onClick={() => alert('button click')}>
         Button
       </button>
